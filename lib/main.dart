dd98bea81c782fc8db6973d0adbc4103ecdba791
FreshDev <billyhans90@gmail.com>
2022-07-16 22:26:02 +0100

    UI almost done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Quizzler());
+  runApp(const Quizzler());
 }
 
 class Quizzler extends StatelessWidget {
@@ -12,10 +12,10 @@ class Quizzler extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.grey.shade900,
-        body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10.0),
-            child: QuizPage(),
+        body: const SafeArea(
+          child: const Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: const QuizPage(),
           ),
         ),
       ),
@@ -36,8 +36,9 @@ class _QuizPageState extends State<QuizPage> {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: const [
-        Expanded(
+      children: [
+        const Expanded(
+          flex: 5,
           child: Padding(
             padding: EdgeInsets.all(10.0),
             child: Center(
@@ -46,6 +47,44 @@ class _QuizPageState extends State<QuizPage> {
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: ElevatedButton(
+              onPressed: null,
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green,
+              ),
+              child: const Text(
+                'True',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: ElevatedButton(
+              onPressed: null,
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red,
+              ),
+              child: const Text(
+                'False',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20.0,
                   color: Colors.white,
                 ),
               ),
